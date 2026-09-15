4a8cdbd91e8302394d6a958845703ce75247a8fa
Rohan Jantre <rohanjantre2004@gmail.com>
2026-09-15 21:42:16 +0530

    cors error solve

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: [
+      "http://localhost:5173",
+      "https://noviqweb.vercel.app"
+    ]
   })
 );
 
